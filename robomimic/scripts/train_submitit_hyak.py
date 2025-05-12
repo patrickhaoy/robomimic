c8813ab6179808d1e8943b37ed8550672799cf5a
Patrick Yin <patyin@cs.washington.edu>
2025-05-12 16:43:00 -0700

    dp longer seq length

--- robomimic/scripts/train_submitit_hyak.py
+++ robomimic/scripts/train_submitit_hyak.py
@@ -40,7 +40,7 @@ def job_fn(job_cfg: ArgumentsConfig):
     sys.argv = [
         "script.py",  # dummy placeholder for program name
         "--config", f"robomimic/exps/templates/{job_cfg.config}.json",
-        "--name", f"{job_cfg.config}_nopad_v3",
+        "--name", f"{job_cfg.config}_s16_v3",
         "--dataset", f"/tmp/datasets_{job_cfg.config}"
     ]
 
