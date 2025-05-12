d607d63f845e1c902b1869fe65d1e45e61dcb40f
Patrick Yin <patyin@cs.washington.edu>
2025-05-11 17:39:40 -0700

    dp exp

--- robomimic/scripts/train_submitit_hyak.py
+++ robomimic/scripts/train_submitit_hyak.py
@@ -11,7 +11,7 @@ class ExampleExecutorConfig(SubmititExecutorConfig):
     slurm_partition: str = "gpu-l40s"
     root_folder: str = "logging_dir"
     cpus_per_task: int = 16
-    mem_gb: int = 200
+    mem_gb: int = 160
     slurm_gpus_per_node: str = "1" # this is saying we want 1 gpu per node
     # slurm_constraint:str = "l40s" # This is saying we need a node with these gpus
 
@@ -19,7 +19,7 @@ class ExampleExecutorConfig(SubmititExecutorConfig):
 class ArgumentsConfig(BaseJobConfig):
     config: str = "dp_rgb"
 
-job_configs = [ArgumentsConfig(config=config) for config in ["mlp_rgb", "mlp_rgb_cnn", "mlp_rgb_r3m", "mlp_rgb_dinov2", "mlp_rgb_r3m_finetune", "dp_rgb"]]
+job_configs = [ArgumentsConfig(config=config) for config in ["dp_rgb"]] # "mlp_rgb", "mlp_rgb_cnn", "mlp_rgb_r3m", "mlp_rgb_dinov2", "mlp_rgb_r3m_finetune", 
 
 # # Since we do not need any checkpointing functionality, and the jobs will 
 # # not use the checkpoint path at all, we can use the base config.
