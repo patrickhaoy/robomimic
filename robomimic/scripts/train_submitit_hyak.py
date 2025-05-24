e7cbc60a10dc1faec62c9efdb95c212fe3206881
Patrick Yin <patyin@cs.washington.edu>
2025-05-23 19:04:25 -0700

    nit

--- robomimic/scripts/train_submitit_hyak.py
+++ robomimic/scripts/train_submitit_hyak.py
@@ -8,10 +8,10 @@ from submitit_tools.configs import SubmititExecutorConfig, BaseJobConfig, WandbC
 @dataclass
 class ExampleExecutorConfig(SubmititExecutorConfig):
     timeout_min: int = 48 * 60
-    slurm_partition: str = "gpu-l40s"
+    slurm_partition: str = "gpu-l40"
     root_folder: str = "logging_dir"
-    cpus_per_task: int = 16
-    mem_gb: int = 200
+    cpus_per_task: int = 14
+    mem_gb: int = 160
     slurm_gpus_per_node: str = "1" # this is saying we want 1 gpu per node
     # slurm_constraint:str = "l40s" # This is saying we need a node with these gpus
 
@@ -19,7 +19,7 @@ class ExampleExecutorConfig(SubmititExecutorConfig):
 class ArgumentsConfig(BaseJobConfig):
     config: str = "dp_rgb"
 
-job_configs = [ArgumentsConfig(config=config) for config in ["mlp_rgb", "mlp_rgb_r3m", "mlp_rgb_dinov2", "dp_rgb"]]
+job_configs = [ArgumentsConfig(config=config) for config in ["mlp_rgb_nohistory", "mlp_rgb", "mlp_rgb_r3m", "mlp_rgb_dinov2", "dp_rgb"]]
 
 # # Since we do not need any checkpointing functionality, and the jobs will 
 # # not use the checkpoint path at all, we can use the base config.
